c147887d3a2440f708cdcaec16ab2c45405c6e43
cormacio100 <cormac.music@gmail.com>
2017-12-13 22:32:46 +0000

    Username Uniqueness added and improvements made to error messaging

--- accounts/views.py
+++ accounts/views.py
@@ -8,34 +8,45 @@ from django.template.context_processors import csrf
 from accounts.forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-
+from accounts.models import User
 
 # VIEW TO DISPLAY REGISTER FORM
 def auth_register(request):
     #   ONCE THE REGISTRATION FORM IS SUBMITTED
     if request.method == 'POST':
-        #   retrieve values from CUSTOM FORM
-        form = UserRegistrationForm(request.POST)
-        #   save the form if it is valid
-        if form.is_valid():
-            form.save()
-            #   AUTHENTICATE THE USER BASED ON EMAIL AND PASSWORD PASSED IN
-            #   using authentication defined in function in backends.py file
-            user = auth.authenticate(email=request.POST.get('email'),
-                                     password=request.POST.get('password1'))
-            #   Log the user in and show their profile
-            if user:
-                login(request,user)
-                #   Check user_type the new user is
-                if request.POST.get('account_type') == 'Entertainer':
-                    #messages.success(request, "You have successfully registered as an Entertainer")
-                    return redirect(reverse('entertainers:create_profile'))
-                else:
-                    messages.success(request, "You have successfully registered as a General User")
-                return redirect(reverse('accounts:profile'))
-            else:
-                messages.error(request, "unable to log you in at this time!")
+        #   check that the ass
+        if request.POST['password1']==request.POST['password2']:
 
+            try:
+                user = User.objects.get(username=request.POST['email'])
+                messages.error(request, "Username Already Exists")
+                return redirect(reverse('accounts:register'))
+            except User.DoesNotExist:
+
+                #   retrieve values from CUSTOM FORM
+                form = UserRegistrationForm(request.POST)
+                #   save the form if it is valid
+                if form.is_valid():
+                    form.save()
+                    #   AUTHENTICATE THE USER BASED ON EMAIL AND PASSWORD PASSED IN
+                    #   using authentication defined in function in backends.py file
+                    user = auth.authenticate(email=request.POST.get('email'),
+                                             password=request.POST.get('password1'))
+                    #   Log the user in and show their profile
+                    if user:
+                        login(request,user)
+                        #   Check user_type the new user is
+                        if request.POST.get('account_type') == 'Entertainer':
+                            #messages.success(request, "You have successfully registered as an Entertainer")
+                            return redirect(reverse('entertainers:create_profile'))
+                        else:
+                            messages.success(request, "You have successfully registered as a General User")
+                        return redirect(reverse('accounts:profile'))
+                    else:
+                        messages.error(request, "unable to log you in at this time!")
+        else:
+            messages.error(request, "Passwords do not Match")
+            return redirect(reverse('accounts:register'))
     #   INITIALLY THE METHOD WILL NOT BE EQUAL TO POST SO WILL DISPLAY THE EMPTY FORM
     else:
         form = UserRegistrationForm()
