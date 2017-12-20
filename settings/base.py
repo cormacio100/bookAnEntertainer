293ffd9a83b14a80a1ea4cfba509aab5d6bc42f5
cormacio100 <cormac.music@gmail.com>
2017-12-20 14:27:10 +0000

    heroku all staging setting put into settings/base.py as does not work from staging

--- settings/base.py
+++ settings/base.py
@@ -145,7 +145,43 @@ MEDIA_ROOT = BASE_DIR
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-PAYPAL_RECEIVER_EMAIL = 'cormac.music@gmail.com'
-PAYPAL_NOTIFY_URL = 'https://bookanentertainer.herokuapp.com/to-ngrok-or-not-to-ngrok/'     #   URL taken from name of HEROKU App
+
+########################################################################
+#   STAGING SETTINGS - TAKEN FRO staging.py
+########################################################################
+
+import dj_database_url
+
+DEBUG = False
+
+DATABASES = {
+    #'default': dj_database_url.config('mysql://b75ca10aad0726:fe49e080@eu-cdbr-west-01.cleardb.com/heroku_d28248eb1f8e96b?') # book-an-entertainer
+    #'default': dj_database_url.config('mysql://b04dad47c09741:c839778c@eu-cdbr-west-01.cleardb.com/heroku_0ec0fc4d7ab16c5?') # bookanentertainer
+    'default': dj_database_url.config('mysql://b14829df6b68b7:545112f9@eu-cdbr-west-01.cleardb.com/heroku_d3c5ad404e32217?') # bookanentertainer
+}
+
+ALLOWED_HOSTS.append('bookanentertainer.herokuapp.com')
+
+#   PAYPAL SETTINGS
 SITE_URL = 'http://bookanentertainer.herokuapp.com'
+PAYPAL_NOTIFY_URL = 'https://bookanentertainer.herokuapp.com/to-ngrok-or-not-to-ngrok/'     #   URL taken from name of HEROKU App
+PAYPAL_RECEIVER_EMAIL = 'cormac.music-facilitator@gmail.com'
+
+# Since DEBUG = False, you can log DEBUG information to the HEROKU console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
