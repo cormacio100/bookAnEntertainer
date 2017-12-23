5422729d38778c20728fe0ee541dc879d36f453c
cormacio100 <cormac.music@gmail.com>
2017-12-23 01:44:32 +0000

    static file settings update for heroku6. Images not appearing

--- settings/base.py
+++ settings/base.py
@@ -150,7 +150,7 @@ STATIC_URL = '/static/'
 
 #SEE https://blog.doismellburning.co.uk/django-and-static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 """
 STATICFILES_DIRS IS FOR OUTPUTS
